76e3939c8a4f50a27782c16f7b6d3809a84ec59c
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-11-10 18:18:24 +0200

    add example with insert

---
+++ basic/insert.sql
@@ -0,0 +1,8 @@
+
+insert into weather values ('Kiyv', -6, 14, 0.25, '2024-11-9');
+
+insert into cities values ('Kiyv', '(-194.0, 53.0)');
+
+insert into weather (date, city, temp_hi, temp_lo) values ('2024-11-8', 'Kiyv', 13, -3);
+
+-- copy weather from '/home/user/weather.txt';
